69d54a3e7e82a74791ed8c49e173e94829d186bd
Akshat Singh <Akshat.singh@kratikal.com>
2024-03-20 12:06:40 +0530

    Added parameters description for exported function

--- index.js
+++ index.js
@@ -15,9 +15,18 @@ const { validator , normalizeIO} = require("./src/utils/function");
 const { executeCommand} = require("./src/cli/run");
 const { cleanUp } = require("./src/utils/fileSetup");
 /**
- * 
- * @param {*} param0 
- * @returns 
+ * Asynchronously generates subtitles based on input file and flags.
+ * @param {Object} options - An object containing options for generation.
+ * @param {string} options.inputFile - The path or name of the input file.
+ * @param {string} options.outputDir - The directory where the output will be saved.
+ * @param {string} options.inputType - The type of input data.
+ * @param {Object} options.whisperFlags - An object containing flags for whispering.
+ * @param {string} options.whisperFlags.subFormat - The subtitle format to generate.
+ * @param {string} options.whisperFlags.model - The model to use for generation.
+ * @param {string} options.whisperFlags.language - The language for subtitles.
+ * @param {number} options.whisperFlags.threadCount - The number of threads to use.
+ * @param {boolean} options.enableTraceLogs - A flag to enable trace logs.
+ * @returns {Promise<boolean>} - A promise that resolves true when the file is succesfully generated
  */
 async function generate({ inputFile, outputDir, inputType, whisperFlags, enableTraceLogs}) {
     //! TODO: Add benchmarking flags for user, sending user performance
@@ -46,5 +55,6 @@ async function generate({ inputFile, outputDir, inputType, whisperFlags, enableT
         })
     })
 }
+
 
 module.exports = { generate };
